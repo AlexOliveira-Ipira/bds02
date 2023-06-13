102c969605540030d701296062dd1905baceea25
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2023-06-13 17:31:02 -0300

    Implementando os teste do Event.

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO eventDTO){
+        Optional<City> cityId = cityRepository.findById(eventDTO.getCityId());
+        try{
+            Event event = repository.getOne(id);
+            event.setName(eventDTO.getName());
+            event.setDate(eventDTO.getDate());
+            event.setUrl(eventDTO.getUrl());
+            event.setCity(cityId.get());
+            return new EventDTO(event);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
